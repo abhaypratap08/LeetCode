57835c6efd911573e8840b2a78261da8d9b783f8
abhaypratap08 <pratapsinghabhay0208@gmail.com>
2025-10-18 06:43:05 +0000

    Code challenge submission for question: 50 Pow(x, n)

---
+++ 50 Pow(x, n)/50powx-n.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public double myPow(double x, int n) {
+        long N = n; 
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return Pow(x, N);
+    }
+    private double Pow(double x, long n) {
+        if (n == 0) return 1;
+        double half = Pow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+}
