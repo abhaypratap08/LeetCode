83f32274ad0a725f8dcc3b77c378be20d66f0e64
Abhay Pratap singh <pratapsinghabhay0208@gmail.com>
2026-09-16 08:33:48 +0530

    0494-TARGET-SUM Stats: Time: 6 ms (72.69%), Space: 46.6 MB (29.94%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,3 +1,6 @@
+import java.util.*;
+
+
 //RECURSION
 
 // class Solution {
@@ -22,24 +25,59 @@
 
 //MEMOIZATION
 
-import java.util.*;
+// class Solution {
+//     public int findTargetSumWays(int[] nums, int target) {
+//         int total = 0;
+//         for (int x : nums) total += x;
+//         if (Math.abs(target) > total) return 0;
+//         int[][] dp = new int[nums.length][2 * total + 1];
+//         for (int[] r : dp) Arrays.fill(r, -1);  
+//         return f(0, 0, nums, target, dp, total);
+//     }
+
+//     public int f(int i, int sum, int[] nums, int target, int[][] dp, int offset) {
+//         if (i == nums.length) return sum == target ? 1 : 0; 
+//         int index = sum + offset;
+//         if (dp[i][index] != -1) return dp[i][index];
+//         int plus = f(i + 1, sum + nums[i], nums, target, dp, offset);
+//         int minus = f(i + 1, sum - nums[i], nums, target, dp, offset);
+//         return dp[i][index] = plus + minus;
+//     }
+// }
+
+
+//TABULATION
 
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int total = 0;
-        for (int x : nums) total += x;
-        if (Math.abs(target) > total) return 0;
-        int[][] dp = new int[nums.length][2 * total + 1];
-        for (int[] r : dp) Arrays.fill(r, -1);  
-        return f(0, 0, nums, target, dp, total);
-    }
 
-    public int f(int i, int sum, int[] nums, int target, int[][] dp, int offset) {
-        if (i == nums.length) return sum == target ? 1 : 0; 
-        int index = sum + offset;
-        if (dp[i][index] != -1) return dp[i][index];
-        int plus = f(i + 1, sum + nums[i], nums, target, dp, offset);
-        int minus = f(i + 1, sum - nums[i], nums, target, dp, offset);
-        return dp[i][index] = plus + minus;
+        for (int x : nums) {
+            total += x;
+        }
+
+        if (Math.abs(target) > total) {
+            return 0;
+        }
+
+        int offset = total;
+        int[][] dp = new int[nums.length + 1][2 * total + 1];
+
+        dp[0][offset] = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int sum = -total; sum <= total; sum++) {
+                int ways = dp[i][sum + offset];
+
+                if (ways == 0) {
+                    continue;
+                }
+
+                dp[i + 1][sum + nums[i] + offset] += ways;
+                dp[i + 1][sum - nums[i] + offset] += ways;
+            }
+        }
+
+        return dp[nums.length][target + offset];
     }
 }
